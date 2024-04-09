fdfe51a853c06144efefb108b716ab895053379b
LovelyToaster <lovelytoaster94@outlook.com>
2024-04-09 21:16:57 +0800

    perf: 长时间加载失败的提示界面

--- src/hooks/loading.ts
+++ src/hooks/loading.ts
@@ -1,8 +1,9 @@
-import {ElLoading} from "element-plus";
-import {watch} from "vue";
+import {ElLoading, ElNotification} from "element-plus";
+import {ref, watch} from "vue";
 import {useMusicStore} from "@/store/music";
 
 export function musicLoading() {
+    let time = ref(0)
     const musicStore = useMusicStore()
     const elLoadingOptions = {
         lock: true,
@@ -10,14 +11,27 @@ export function musicLoading() {
     }
     let loading = ElLoading.service(elLoadingOptions)
 
-    watch(() => musicStore.musicReady, () => {
+    watch(() => [musicStore.musicReady, time.value], () => {
         //重新加载
         if (!musicStore.musicReady) {
             loading = ElLoading.service(elLoadingOptions)
         }
         //加载完成，关闭加载界面
-        if (musicStore.musicReady)
+        if (musicStore.musicReady) {
             loading.close()
-
+            if (time.value >= 5) {
+                if (!musicStore.musicReady) {
+                    ElNotification({
+                        message: '加载失败，请尝试刷新！',
+                        type: 'error',
+                    })
+                }
+                clearInterval(stopInterval)
+            }
+        }
     })
+
+    const stopInterval = setInterval(() => {
+        time.value++
+    }, 1000)
 }
