a3bd986960451bf47c249d91113059d3dae41c0b
LovelyToaster <lovelytoaster94@outlook.com>
2024-04-13 10:47:55 +0800

    fix: 修复只在第一次加载出错时显示提示信息

--- src/hooks/loading.ts
+++ src/hooks/loading.ts
@@ -1,37 +1,47 @@
 import {ElLoading, ElNotification} from "element-plus";
-import {ref, watch} from "vue";
+import {watch} from "vue";
 import {useMusicStore} from "@/store/music";
 
-export function musicLoading() {
-    let time = ref(0)
+let time = 0
+
+
+function Interval() {
     const musicStore = useMusicStore()
-    const elLoadingOptions = {
-        lock: true,
-        text: "加载中，请稍后"
-    }
-    let loading = ElLoading.service(elLoadingOptions)
-
-    watch(() => [musicStore.musicReady, time.value], () => {
-        //重新加载
-        if (!musicStore.musicReady) {
-            loading = ElLoading.service(elLoadingOptions)
-        }
-        //加载完成，关闭加载界面
-        if (musicStore.musicReady) {
-            loading.close()
-        }
-    })
-
     const stopInterval = setInterval(() => {
-        time.value++
-        if (time.value >= 3) {
+        time++
+        console.log(time)
+        if (time >= 3) {
             if (!musicStore.musicReady) {
                 ElNotification({
                     message: '加载失败，请尝试刷新！',
                     type: 'error',
                 })
             }
+            time = 0
             clearInterval(stopInterval)
         }
+
     }, 1000)
+}
+
+export function musicLoading() {
+    const musicStore = useMusicStore()
+    const elLoadingOptions = {
+        lock: true,
+        text: "加载中，请稍后"
+    }
+    Interval()
+    let loading = ElLoading.service(elLoadingOptions)
+
+    watch(() => musicStore.musicReady, () => {
+        //重新加载
+        if (!musicStore.musicReady) {
+            loading = ElLoading.service(elLoadingOptions)
+            Interval()
+        }
+        //加载完成，关闭加载界面
+        if (musicStore.musicReady) {
+            loading.close()
+        }
+    })
 }
