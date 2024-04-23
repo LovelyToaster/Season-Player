5023bda02958aeec66c57fe78254a1091a2982b2
DoctorYu <2460972805@qq.com>
2024-04-23 20:21:25 +0800

    修改了头像的内容信息

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -21,7 +21,7 @@ let seasonList = reactive([
   {"seasonName": "清冬", "seasonPath": "/Winter"},
 ]) //所有季节
 
-//退出登录（旧）
+
 /*function backToHome() {
   loginStore.loginOut()
   router.push({name: 'home'});
