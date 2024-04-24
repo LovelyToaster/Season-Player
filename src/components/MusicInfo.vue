57ca5e55e9e6a1767cd57020861754523e4144a2
lovelytoaster94 <lovelytoaster94@outlook.com>
2024-04-24 21:18:23 +0800

    feat: 简易的歌词自动滚动

--- src/components/MusicInfo.vue
+++ src/components/MusicInfo.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import '@/style/backNav.css'
 import {useMusicStore} from "@/store/music";
-import {onUnmounted} from "vue";
+import {onUnmounted, ref, watch} from "vue";
 import {musicLoading} from "@/hooks/loading";
 
 const musicStore = useMusicStore()
 
 let {season} = defineProps(["season"])
+let songWord = ref()
+let row = 0
+let time = 0
 
 musicStore.currentSeason = season
 musicStore.getMusic()
@@ -17,6 +20,25 @@ onUnmounted(() => {
 
 //执行加载界面
 musicLoading()
+
+watch(() => musicStore.currentRow, () => {
+  time++
+  if (musicStore.currentRow >= 4) {
+    songWord.value.scrollTop += 46
+  }
+  if (musicStore.currentRow != row && time >= 2) {
+    if (musicStore.currentRow >= 4)
+      songWord.value.scrollTop = (musicStore.currentRow - 3) * 46
+    else
+      songWord.value.scrollTop = musicStore.currentRow * 46
+    time = 0
+  }
+})
+
+function getScrollTop() {
+  row = Math.round(songWord.value.scrollTop / 46) + 4
+}
+
 </script>
 
 <template>
@@ -27,7 +49,7 @@ musicLoading()
     <!--    歌名-->
     <p class="songName">{{ musicStore.musicInfo.musicName }}</p>
     <!--    歌词-->
-    <div class="songWord">
+    <div class="songWord" ref="songWord" @scroll="getScrollTop">
       <ul>
         <li v-for="(lyric,index) in musicStore.lyricList"
             :style="{color:index===musicStore.currentRow?'red':'white'}">
